3f3af0a5778e24988d7f31708071cc014fb90881
Jack Dunn <jack.dunn@live.co.uk>
2021-12-03 16:20:11 +0000

    Separate out rest API into its own project

---
+++ src/api/ActivityTracker.Web.Rest.Api/Ioc/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using ActivityTracker.Application.Repositories;
+using ActivityTracker.Application.Services;
+using ActivityTracker.Data.Persistence.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ActivityTracker.Web.Rest.Api.Ioc
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void InstallDependencies(this IServiceCollection services)
+        {
+            services.AddSingleton<IDateTimeProvider, DateTimeUtcNowProvider>();
+            services.AddSingleton<IActivityService, ActivityService>();
+            services.AddSingleton<IActivityRepository, InMemoryActivityRepository>();
+        }
+    }
+}
